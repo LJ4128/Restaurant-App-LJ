f66f44dfd0e04627dca5663d888a57e2227fc08d
Jones <1162570@bcpss.org>
2025-11-06 14:45:08 -0500

    Changed to a pizza joint. changed andress into a button which opens up a map app displaying the adress

--- app/src/main/java/org/balitmorecityschools/restaurantapp/MainActivity.java
+++ app/src/main/java/org/balitmorecityschools/restaurantapp/MainActivity.java
@@ -1,6 +1,7 @@
 package org.balitmorecityschools.restaurantapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,19 +13,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button food;
+    Button foodBTN;
+    Button locationBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        food = (Button) findViewById(R.id.foodBTN);
-        food.setOnClickListener(new View.OnClickListener() {
+        foodBTN = (Button) findViewById(R.id.foodBTN);
+        locationBTN = findViewById(R.id.locationBTN);
+        foodBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent foodScreen = new Intent(MainActivity.this, FoodBrowser.class);
                 startActivity(foodScreen);
             }
         });
+        locationBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewMap = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:39.2905742,-76.5374844,16?z=15"));
+                startActivity(viewMap);
+
+            }
+        });
 
     }
+
 }
